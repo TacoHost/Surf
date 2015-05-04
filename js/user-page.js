b5a61f70ba16e8d94358912d5547ff03deb52551
Denis Ryaboshtanov <tacohost@gmail.com>
2015-05-04 14:50:30 +0300

    user-page wallpaper fix

--- js/user-page.js
+++ js/user-page.js
@@ -1,2 +1,7 @@
-﻿var wallpaperWidth = $(".wallpaper-block").css("width"); //taking width of wallpaper
-$(".js-blurred-image").css("width", wallpaperWidth); //implementing width to blurred image
+﻿function blurImage() {
+    var wallpaperWidth = $(".wallpaper-block").css("width"); //taking width of wallpaper
+    $(".js-blurred-image").css("width", wallpaperWidth); //implementing width to blurred image
+}
+$(window).resize(function () {
+    blurImage();
+});
