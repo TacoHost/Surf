f2fef2675ee26394a582e7205ad1b5525ac78442
Denis Ryaboshtanov <tacohost@gmail.com>
2015-05-05 10:25:44 +0300

    tooltip/user page/article page fixes

---
+++ js/layout.js
@@ -0,0 +1,13 @@
+﻿$('[data-toggle="tooltip"]').tooltip();
+
+var distanceToSocial = $('.social-block').offset().top;
+
+$(document).scroll(function() {
+    var fromTop = ($(document).scrollTop());
+
+    if ($(document).scrollTop() >= distanceToSocial) {
+        $('.social-block').css("top", fromTop);
+    } else {
+        return false;
+    }
+});
--- js/user-page.js
+++ js/user-page.js
@@ -5,13 +5,3 @@
 $(window).resize(function () {
     blurImage();
 });
-
-var distanceToSocial = $('.social-block').offset().top, $window = $(window);
-
-$window.scroll(function () {
-    if ($window.scrollTop() >= distanceToSocial) {
-        $('.social-block').css("top", "20px");
-    } else {
-        $('.social-block');
-    }
-});
