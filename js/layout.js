af14cf6d75e35cfe7b5dccd74665e0f7593dd0b0
Max Nadolsky <maximusdreddoff@gmail.com>
2015-05-05 10:47:48 +0300

    scroll-script

--- js/layout.js
+++ js/layout.js
@@ -1,4 +1,4 @@
-﻿$('[data-toggle="tooltip"]').tooltip();
+﻿/*$('[data-toggle="tooltip"]').tooltip();
 
 var distanceToSocial = $('.social-block').offset().top;
 
@@ -10,4 +10,18 @@ $(document).scroll(function() {
     } else {
         return false;
     }
+});*/
+$(window).scroll(function() {
+    $(window).scroll(function() {
+        if (window.scrollY >= 600) {
+            $(".social-block").css("position", "fixed").css("top", "50px");
+            console.log("HEHLELELELELELLELE");
+        }
+        if (window.scrollY < 600) {
+            $(".social-block").css({
+                position: "absolute",
+                top: "0"
+        });
+        }
+    });
 });
