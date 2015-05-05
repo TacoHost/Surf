64619a4240912243c5004fa16d8840b1ba77e5ce
Max Nadolsky <maximusdreddoff@gmail.com>
2015-05-05 12:30:17 +0300

    WorkingWithScript

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,4 @@
-﻿/*$('[data-toggle="tooltip"]').tooltip();
-
+﻿/*
 var distanceToSocial = $('.social-block').offset().top;
 
 $(document).scroll(function() {
@@ -10,18 +9,48 @@ $(document).scroll(function() {
     } else {
         return false;
     }
-});*/
-$(window).scroll(function() {
-    $(window).scroll(function() {
-        if (window.scrollY >= 600) {
-            $(".social-block").css("position", "fixed").css("top", "50px");
-            console.log("HEHLELELELELELLELE");
+});
+
+$(window).scroll(function () {
+    var $socblock = $(".social-block");
+    var top = $socblock.offset().top - parseFloat($socblock.css("margin-top").replace(/auto/, 0));;
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        if (y >= top) {
+            $socblock.addClass("stuck");
         }
-        if (window.scrollY < 600) {
-            $(".social-block").css({
-                position: "absolute",
-                top: "0"
-        });
+        else {
+            $socblock.removeClass("stuck");
         }
     });
+});   FIX OR DELETE*/ 
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+    var $socblock = $(".social-block");
+    $(window).scroll(function() {
+
+        if (window.scrollY >= $socblock.offset().top) {
+            $socblock.css("position", "fixed").css("top", "50px");
+        }
+        if (window.scrollY < $socblock.offset().top) {
+            $socblock.css({
+                position: "absolute",
+                top: "0"
+            });
+        }
+    });
+    /*
+        var $socblock = $(".social-block"),
+            $window = $(window),
+            offset = $socblock.offset();
+
+        $(window).scroll(function() {
+            if ($window.scrollTop() >= offset.top) {
+                $socblock.addClass("fixed");
+
+            } else {
+                $socblock.removeClass("fixed");
+
+            }
+        });FIX OF DELETE*/
 });
