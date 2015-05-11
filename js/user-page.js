fbf73e6d8e43fc8704249b9c0d333d9b988c7ce2
Max Nadolsky <maximusdreddoff@gmail.com>
2015-05-11 19:38:07 +0300

    community-page-script

--- js/user-page.js
+++ js/user-page.js
@@ -1,7 +1,10 @@
-﻿function blurImage() {
-    var wallpaperWidth = $(".wallpaper-block").css("width"); //taking width of wallpaper
-    $(".js-blurred-image").css("width", wallpaperWidth); //implementing width to blurred image
-}
-$(window).resize(function () {
+﻿$(document).ready(function() {
+    function blurImage() {
+        var wallpaperWidth = $(".wallpaper-block").css("width"); //taking width of wallpaper
+        $(".js-blurred-image").css("width", wallpaperWidth); //implementing width to blurred image
+    }
     blurImage();
+    /*$(window).resize(function() {
+        blurImage();
+    }); the old code*/
 });
