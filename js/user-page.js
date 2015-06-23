936d6daa7e184a6818737798d01c3d2450eb0264
Max Nadolsky <maximusdreddoff@gmail.com>
2015-06-23 14:41:50 +0300

    reborn

--- js/user-page.js
+++ js/user-page.js
@@ -1,10 +1,7 @@
-﻿$(document).ready(function() {
-    function blurImage() {
-        var wallpaperWidth = $(".wallpaper-block").css("width"); //taking width of wallpaper
-        $(".js-blurred-image").css("width", wallpaperWidth); //implementing width to blurred image
-    }
+﻿function blurImage() {
+    var wallpaperWidth = $(".wallpaper-block").css("width"); //taking width of wallpaper
+    $(".js-blurred-image").css("width", wallpaperWidth); //implementing width to blurred image
+}
+$(window).resize(function () {
     blurImage();
-    /*$(window).resize(function() {
-        blurImage();
-    }); the old code*/
 });
